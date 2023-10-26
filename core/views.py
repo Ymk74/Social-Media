25136fcc2d056e28f9126b857cbadc8433353731
ymk74 <swimmer.shark12@gmail.com>
2023-10-26 15:10:11 +0300

    create logout views

--- core/views.py
+++ core/views.py
@@ -61,3 +61,8 @@ def signin(request):
             return redirect('signin')
     else: 
         return render(request, 'signin.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
